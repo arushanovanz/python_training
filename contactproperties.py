937b67b0a49b59b9d477d55defa33f9ddf6a1ee8
arushanovanz <nadin.spb@gmail.com>
2019-06-02 23:26:15 +0300

    Created add_contact and contactproperties classes Test cant load contact photo yet

---
+++ contactproperties.py
@@ -0,0 +1,32 @@
+class ContactProperties:
+
+    def __init__(self, firstname, middlename, lastname,nickname,photo,title,company,address,home,mobile,work,fax,
+                 email,email2,email3,homepage,bday,bmonth,byear,aday,amonth,ayear,address2,phone2,notes):
+        self.firstname = firstname,
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.photo = photo
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home = home
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
+
+
+
