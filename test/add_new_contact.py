4d34b7482261cf791904b0240668b677a7b4201b
arushanovanz <nadin.spb@gmail.com>
2019-06-09 21:53:50 +0300

    Extract class ContactHelper

--- test/add_new_contact.py
+++ test/add_new_contact.py
@@ -15,7 +15,7 @@ def test_add_new_contact(app):
     app.session.login( username="admin", password="secret")
     # photo path
     # p= os.path.abspath('C:\Users\hh\Pictures\Git.jpeg')
-    app.create_new_contact(ContactProperties(firstname="Andrew", middlename="Ivanovich", lastname="Lobachev", nickname="green",
+    app.contact.create(ContactProperties(firstname="Andrew", middlename="Ivanovich", lastname="Lobachev", nickname="green",
                                                       photo= "",title="Title1",
                                                       company="Company1",address= "Adress1 Company1",home="89779087650",mobile="89779087651",
                                                       work="89779087652",fax="89779087653",email="andrew1@tt.ru",email2="andrew2@tt.ru",
