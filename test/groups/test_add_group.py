b84fd079f88fad953dd0eb3157324c792273e309
arushanovanz <nadin.spb@gmail.com>
2019-06-30 17:59:45 +0300

    Added test assertions of lists' length for groups

--- test/groups/test_add_group.py
+++ test/groups/test_add_group.py
@@ -2,10 +2,16 @@
 from model.group import Group
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="yfpdfybt uhes",header="header",footer="yjdfz uhef 1"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups)+ 1 == len(new_groups)
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name=" ",header=" ",footer=" "))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 
--- test/groups/test_edit_group.py
+++ test/groups/test_edit_group.py
@@ -3,14 +3,23 @@ from model.group import Group
 def test_edit_group_name(app):
     if app.group.count() ==0:
         app.group.create(Group(name="test edit group"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="Новая модифицированная группа2"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_edit_group_header(app):
     if app.group.count() ==0:
         app.group.create(Group(header="test Header group"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(header="New edited Header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_edit_group_footer(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() ==0:
         app.group.create(Group(footer="test Header footer"))
     app.group.edit_first_group(Group(footer="New edited footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
