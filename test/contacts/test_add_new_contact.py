49c8a339d2983804d4ed63756abd2e8016fe5376
arushanovanz <nadin.spb@gmail.com>
2019-07-18 18:12:27 +0300

    fixed parametrized values

--- test/contacts/test_add_new_contact.py
+++ test/contacts/test_add_new_contact.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from model.group import Group
 import pytest
 import random
 import string
@@ -10,23 +9,16 @@ def random_string(prefix,maxlen):
     symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-def random_month(self):
+def random_month():
     months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
-    return random.choice((months))
+    return random.choice(months)
 
 def random_phones(prefix,maxlen):
     symbols =  string.digits + string.punctuation
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
-testdata =[ContactProperties (firstname="", middlename="", lastname="", nickname="",
-                                                      photo= "",title="",
-                                                      company="",address= "",homephone="",mobilephone="",
-                                                      workphone="",fax="",email="",email2="",
-                                                      email3="",homepage="",address2="",
-                                                      secondaryphone="",notes="",ayear="",amonth="",aday ="",
-                                                      byear="",bmonth="",bday="")] +\
-          [ ContactProperties (firstname=random_string("firstname",20),
+testdata =[ ContactProperties (firstname=random_string("firstname",20),
                                middlename=random_string("middlename",20),
                                lastname=random_string("lastname",20),
                                nickname=random_string("nickname",10),
@@ -46,22 +38,23 @@ testdata =[ContactProperties (firstname="", middlename="", lastname="", nickname
                                secondaryphone=random_phones("",11),
                                notes=random_string("Notes",40),
                                ayear= random.randint(1945,2019),
-                               amonth=random_month,
+                               amonth= random_month(),
                                aday =random.randint(1,31),
                                byear= random.randint(1945,2019),
-                               bmonth=random_month,
-                               bday=random.randint(1,31))
+                               bmonth= random_month(),
+                               bday=random.randint(1,31)
+                               )
+             for i in range(5)
             ]
 
 
 @pytest.mark.parametrize("contact",testdata, ids=[repr (x)for x in testdata])
 def test_add_new_contact(app,contact):
-      pass
-    # # photo path
+    # photo path
     # # p= os.path.abspath('C:\Users\hh\Pictures\Git.jpeg')
-    # old_contacts = app.contact.get_contact_list()
-    # app.contact.create(contact)
-    # new_contacts = app.contact.get_contact_list()
-    # assert len(old_contacts) + 1 == app.contact.count()
-    # old_contacts.append(contact)
-    # assert sorted(old_contacts, key=ContactProperties.id_or_max) == sorted(new_contacts, key=ContactProperties.id_or_max)
+        old_contacts = app.contact.get_contact_list()
+        app.contact.create(contact)
+        new_contacts = app.contact.get_contact_list()
+        assert len(old_contacts) + 1 == app.contact.count()
+        old_contacts.append(contact)
+        assert sorted(old_contacts, key=ContactProperties.id_or_max) == sorted(new_contacts, key=ContactProperties.id_or_max)
