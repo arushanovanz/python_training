b3f9a4d38ec81d1ce6a39a3e19a3c21adcfeb57e
arushanovanz <nadin.spb@gmail.com>
2019-07-18 20:13:49 +0300

    Added load options from configuration file

--- conftest.py
+++ conftest.py
@@ -1,21 +1,24 @@
-
+import os.path
 import pytest
+import json
 from fixture.application import Application
 
 fixture = None
+target = None
 
 # initialize fixture
 @pytest.fixture
 def app(request):
     global  fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    if fixture is None:
-        fixture = Application(browser= browser,base_url = base_url)
-    else:
-        if not  fixture.is_valid():
-            fixture = Application(browser= browser,base_url = base_url)
-    fixture.session.ensure_login(username="admin", password="secret")
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+             target = json.load(f)
+    if fixture is None or fixture.is_valid():
+        fixture = Application(browser= browser,base_url = target['baseUrl'])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 @pytest.fixture(scope="session",autouse=True)
@@ -29,4 +32,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action ="store",default = "firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--target", action="store", default="target.json")
