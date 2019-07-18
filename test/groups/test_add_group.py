45d340ba429c30e28d47e910d25db9a542c9f827
arushanovanz <nadin.spb@gmail.com>
2019-07-18 20:55:00 +0300

    Added load options from configuration file

--- test/groups/test_add_group.py
+++ test/groups/test_add_group.py
@@ -1,19 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 import pytest
-import random
-import string
-
-
-def random_string(prefix,maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Group(name="", header="", footer="")]+[
-           Group(name=random_string("name",10), header=random_string("header",20), footer=random_string("footer",20))
-           for i in range(5)
-    ]
+from data.groups import testdata
 
 @pytest.mark.parametrize("group",testdata, ids=[repr (x)for x in testdata])
 def test_add_group(app,group):
